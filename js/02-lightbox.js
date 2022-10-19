a2448e62b2b04a7c80d4b44ff35e59075b31f702
al99vin <al99vin1@gmail.com>
2022-10-19 20:51:45 +0300

    2 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+// list of items and pull down
+
+
+
+const marked = galleryItems.map((image)=> `
+    <a class = "gallery__item" href = '${image.original}'>
+        <img  class = "gallery__image",
+        src = '${image.preview}', 
+        alt = '${image.description}', 
+        data-source = '${image.original}'>
+</a>`).join(""); 
+const itemsContainer = document.querySelector(".gallery"); 
+itemsContainer.insertAdjacentHTML('afterBegin', marked)
 console.log(galleryItems);
+
+//making modal window
+
+var lightbox = new SimpleLightbox('.gallery__item',{captionsData: 'alt', captionDelay: '250',});
+
+console.log(lightbox)
+
+
+
+
+  
