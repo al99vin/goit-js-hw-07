592a8156f21f897287e25c0e6562bbb8489f30a6
al99vin <al99vin1@gmail.com>
2022-10-19 20:04:19 +0300

    1st done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+// list of items and pull down
+
+
+
+const marked = galleryItems.map((image)=> `
+<div class = "gallery__item">
+    <a class = "gallery__link" href = '${image.original}'>
+        <img  class = "gallery__image",
+        src = '${image.preview}', 
+        alt = '${image.description}', 
+        data-source = '${image.original}'>
+</a></div>`).join(""); 
+const itemsContainer = document.querySelector(".gallery"); 
+itemsContainer.insertAdjacentHTML('afterBegin', marked)
 console.log(galleryItems);
+
+//making modal window
+
+function onClick(e){
+        e.preventDefault();
+        if(e.target === e.currentTarget){return;};
+    document.body.addEventListener('keydown', (e) => {
+        if (e.key === "Escape" && instance) instance.close()
+      })
+    const currentImage = e.target;
+    console.log(`"Current image:${currentImage.dataset.source}"`)
+    const instance = basicLightbox.create(`
+            <div class = "modal">
+                <img src = "${currentImage.dataset.source}">
+            </div>`)
+            instance.show();
+    {once: true}
+}
+itemsContainer.addEventListener('click',  onClick);
+
+
+  
