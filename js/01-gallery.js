b08b1d7f58cb28dcf0e46bc3c7178a72c6068b45
al99vin <al99vin1@gmail.com>
2022-10-20 21:05:33 +0300

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -11,6 +11,7 @@ const marked = galleryItems.map((image)=> `
         <img  class = "gallery__image",
         src = '${image.preview}', 
         alt = '${image.description}', 
+        data-source = '${image.original}'>
 </a></div>`).join(""); 
 const itemsContainer = document.querySelector(".gallery"); 
 itemsContainer.insertAdjacentHTML('afterBegin', marked)
